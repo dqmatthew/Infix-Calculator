16485be9373d8b031b7647bd5a60ded1e1c65b29
Matthew Deng <15madeng@gmail.com>
2016-11-30 01:00:50 -0500

    Update LinkedQueue.java

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,11 +1,6 @@
 import java.util.NoSuchElementException;
 
-/* Author: Matthew Deng
- * Course: CSC 172
- * Lab Session: MW 1525-1640
- * Lab Assignment: Project 2
- * Date: Feb. 20th, 2016
- */
+
 public class LinkedQueue implements Queue {
 	private class Node {
 		public Object data;
