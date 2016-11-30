8f90e1916d8de7397ef3c940ef7a8b38aaf0323a
Matthew Deng <15madeng@gmail.com>
2016-11-30 01:01:01 -0500

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,11 +1,4 @@
 
-/* Author: Matthew Deng
- * Course: CSC 172
- * Lab Session: MW 1525-1640
- * Lab Assignment: Project 2
- * Date: Feb. 20th, 2016
- */
-
 public interface Queue {
 
 	void enqueue(Object item);
