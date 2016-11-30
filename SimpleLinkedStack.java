8607df2e921ed2b36c4c2c5bd7d08f71171447ce
Matthew Deng <15madeng@gmail.com>
2016-11-30 01:03:17 -0500

    Update SimpleLinkedStack.java

--- SimpleLinkedStack.java
+++ SimpleLinkedStack.java
@@ -1,12 +1,5 @@
-
 import java.util.NoSuchElementException;
 
-/* Author: Matthew Deng
- * Course: CSC 172
- * Lab Session: MW 1525-1640
- * Lab Assignment: Project 2
- * Date: Feb. 20th, 2016
- */
 public class SimpleLinkedStack implements Stack{
 	  private class Node {
 	        public Object data;
